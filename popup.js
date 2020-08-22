a9b40642d4abae255085d05e9783dde76dd225bc
Sven Gowal <908515+sgowal@users.noreply.github.com>
2020-08-22 17:15:04 +0100

    Reorganization of code.

--- popup.js
+++ popup.js
@@ -1,18 +1,50 @@
 var last_data_received = null;
+var current_url = null;
 
 chrome.runtime.onMessage.addListener(function(request, sender) {
-  if (request.action == "getBibtex") {
-    var bibtex_text = document.getElementById("bibtex");
-    bibtex_text.innerText = request.data.bibtex;
-
-    var link_text = document.getElementById("link");
-    link_text.innerHTML = request.data.url + ": " + request.data.title;
-
-    last_data_received = request.data;
+  if (request.action == "citation-response") {
+    if ("url" in request.data) {
+      var bibtex = bibtex_build(request.data.type,
+                                request.data.title,
+                                request.data.authors,
+                                request.data.journal_or_conference,
+                                request.data.year,
+                                request.data.url,
+                                current_url);
+      var bibtex_text = document.getElementById("bibtex");
+      bibtex_text.innerText = bibtex;
+      var link_text = document.getElementById("link");
+      link_text.innerHTML = request.data.url + ": " + request.data.title;
+      last_data_received = request.data;
+      document.getElementById("buttons").style.display = "inline";
+    } else {
+      var status_text = document.getElementById("status");
+      status.innerHTML = request.data.message;
+    }
   }
 });
 
-function onWindowLoad() {
+function execute_scripts(tab_id, inject_details) {
+  function create_callback(tab_id, inject_detail, inner_callback) {
+    return function () {
+      chrome.tabs.executeScript(tab_id, inject_detail, inner_callback);
+    };
+  }
+  var callback = function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error.
+    if (chrome.runtime.lastError) {
+      var bibtex_text = document.getElementById("bibtex");
+      bibtex_text.innerHTML = "This is not recognized as a paper.\nTry on https://arxiv.org/abs/ pages.\nFor example: <a href='https://arxiv.org/abs/2005.13537'>https://arxiv.org/abs/2005.13537</a>";
+    }
+  };
+  for (var i = inject_details.length - 1; i >= 0; --i)
+    callback = create_callback(tab_id, inject_details[i], callback);
+  if (callback !== null)
+    callback();
+}
+
+
+function on_window_load() {
 
   document.getElementById("bibtex-btn").addEventListener("click", function(event) {
     var bibtex_text = document.getElementById("bibtex");
@@ -21,6 +53,7 @@ function onWindowLoad() {
     window.getSelection().addRange(range);
     document.execCommand("copy");
     window.getSelection().removeAllRanges();
+    document.getElementById("status").innerText = "BibTex copied.";
   });
 
   document.getElementById("link-btn").addEventListener("click", function(event) {
@@ -30,6 +63,7 @@ function onWindowLoad() {
     window.getSelection().addRange(range);
     document.execCommand("copy");
     window.getSelection().removeAllRanges();
+    document.getElementById("status").innerText = "Link copied.";
   });
 
   document.getElementById("zotero-btn").addEventListener("click", function(event) {
@@ -60,24 +94,16 @@ function onWindowLoad() {
     }
   });
 
-  var bibtex_text = document.getElementById("bibtex");
-  var link_text = document.getElementById("link");
+  chrome.tabs.query({"active": true, "windowId": chrome.windows.WINDOW_ID_CURRENT}, function(tabs) {
+    current_url = JSON.stringify(tabs[0].url);
 
-  chrome.tabs.query({"active": true, "windowId": chrome.windows.WINDOW_ID_CURRENT}, function(tabs){
-    var url = JSON.stringify(tabs[0].url);
-    chrome.tabs.executeScript(null, {
-      code: "var current_url = " + url + ";"
-    }, function() {
-      chrome.tabs.executeScript(null, {
-        file: "get_bibtex.js"
-      }, function() {
-        // If you try and inject into an extensions page or the webstore/NTP you'll get an error.
-        if (chrome.runtime.lastError) {
-          bibtex_text.innerHTML = "This is not recognized as a paper.\nTry on https://arxiv.org/abs/ pages.\nFor example: <a href='https://arxiv.org/abs/2005.13537'>https://arxiv.org/abs/2005.13537</a>";
-        }
-      });
-    });
+    execute_scripts(null, [
+        {code: "var current_url = " + current_url + ";"},
+        {file: "src/domains.js"},
+        {file: "src/extensions/ieeexplore.js"},
+        {file: "src/extractor.js"},
+    ]);
   });
 }
 
-window.onload = onWindowLoad;
+window.onload = on_window_load;
